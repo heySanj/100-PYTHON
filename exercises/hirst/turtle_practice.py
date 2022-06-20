9416a1245811a4ff8f0235906f953eb260e80328
heySanj <saprasad92@gmail.com>
2022-06-20 12:57:07 +1000

    Turtle graphics practice

--- exercises/hirst/turtle_practice.py
+++ exercises/hirst/turtle_practice.py
@@ -10,11 +10,19 @@ clear()
 
 from turtle import Turtle, Screen
 import heroes
+import random
 
 timmy = Turtle()
 screen = Screen()
+screen.colormode(255)
 
 # Functions
+def random_color():
+    '''Make Timmy a random color'''
+    timmy.pencolor( random.randint(0, 255),
+                    random.randint(0, 255),
+                    random.randint(0, 255))
+
 def timmy_square(size):
     '''Draw a square of size'''
     timmy.begin_fill()
@@ -22,15 +30,41 @@ def timmy_square(size):
         timmy.forward(size)
         timmy.right(90)
     timmy.end_fill()
-        
+
+def dash_line(dash, length):
+    '''Draw a line of length, with adjustable dash size'''
+    for _ in range(length):
+        timmy.forward(dash)
+        timmy.pu()
+        timmy.forward(dash)
+        timmy.pd()      
+
+def all_shapes(sides, size):
+    '''Draw all polygons up to the number of sides'''
+    for i in range(3, sides):
+        # Change colour
+        random_color()
+        for _ in range(i):
+            timmy.forward(size)
+            timmy.right(360 / i)
+        i += 1
+
+def random_walk(size, steps):
+    '''Randomly move in a direction for x amount of steps'''
+    direction = [0, 90, 180, 270]
+    for _ in range(steps):
+        # Choose random direction
+        timmy.setheading(random.choice(direction))
+        # Colour and move
+        random_color()
+        timmy.forward(size)
 
 # Change shape and settings
-timmy.shape('turtle')
-timmy.color('coral')
+timmy.pensize(15)
+timmy.speed(8)
         
 # Do something
-timmy_square(30)
-print(heroes.gen())
+random_walk(30, 150)
 
 # Exit on click at the end of the code
 screen.exitonclick()
