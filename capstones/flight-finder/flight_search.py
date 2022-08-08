270c6e0d49a58a2066aa1d76ddefca8a67180599
heySanj <saprasad92@gmail.com>
2022-08-08 18:58:44 +1000

    Flight search capability

--- capstones/flight-finder/flight_search.py
+++ capstones/flight-finder/flight_search.py
@@ -10,6 +10,9 @@ class FlightSearch:
         self.HEADERS = {
             "apikey": self.TEQUILA_KEY
         }
+        self.currency = "AUD"
+        self.min_nights = 5
+        self.max_nights = 30
     
     # Return the city code of input city
     def get_city_code(self, city: str) -> str:
@@ -23,3 +26,20 @@ class FlightSearch:
         code = response.json()['locations'][0]['code']
                 
         return(code)
+    
+    def search(self, depart, arrive, date_from, date_to, max_price):
+        search_ep = f"{self.TEQUILA_ENDPOINT}/v2/search"
+        
+        parameters = {
+            "fly_from": depart,
+            "fly_to": arrive,
+            "date_from": date_from,
+            "date_to": date_to,
+            "price_to": max_price,
+            "curr": self.currency,
+            "nights_in_dst_from": self.min_nights,
+            "nights_in_dst_to": self.max_nights
+        }
+        
+        response = requests.get(url=search_ep, headers=self.HEADERS, params=parameters)
+        return(response.json())
--- capstones/flight-finder/main.py
+++ capstones/flight-finder/main.py
@@ -16,28 +16,58 @@ from flight_search import FlightSearch
 from notification_manager import NotificationManager
 
 import json
+from datetime import datetime as dt
+from dateutil.relativedelta import relativedelta
 
+HOME_AIRPORT = "BNE"
 
 # ---------------------------- MAIN PROGRAM ------------------------------- # 
-
-
 
 '''
 Use the Flight Search and Sheety API to populate your own copy of the Google Sheet
 with International Air Transport Association (IATA) codes for each city. Most of the
 cities in the sheet include multiple airports, you want the city code (not the airport code see here).
 '''
-sheet = DataManager()
-searcher = FlightSearch()
+sheet = DataManager() # Establish a connection to the Google sheets
+searcher = FlightSearch() # Establish a connection to the flight search engine
 
-# Populate the sheets IATA codes
+tomorrow = dt.today() + relativedelta(days=+1)
+six_months = tomorrow + relativedelta(months=+6)
+
+# Loop through the Google sheet data
 for location in sheet.data:
+    
+    # If there is no IATA code present -> Generate it through the search engine
     if location['iataCode'] == "":
-        # print(searcher.get_city_code(location['city']))
         code = searcher.get_city_code(location['city'])
         sheet.put_data(location['id'],"iataCode",code)
         
+    # Check for the cheapest flights from Tomorrow to 5 months later
+    flights = searcher.search(HOME_AIRPORT, location['iataCode'], tomorrow.strftime("%d/%m/%Y"), six_months.strftime("%d/%m/%Y"), location['lowestPrice'])['data']
+    
+    # If cheaper flights are found
+    if len(flights) > 0:
+        flight = FlightData(flights[0]) # get the first flight
+        print(flight.get_details())
+    
+    
+    
+    # flights_list = []
 
-# data = searcher.get_city_code("tokyo")
+    # for flight in flights:
+    #     new_flight = FlightData(flight)
+    #     flights_list.append(new_flight)
+    #     print(new_flight.get_details())
+    
+    
+    # If the price is lower than the one stored on the sheet -> send an alert!
+        
 
-# print(json.dumps(data, indent=4, sort_keys=True))
+
+
+
+
+
+
+
+# print(json.dumps(flights, indent=4, sort_keys=True))
