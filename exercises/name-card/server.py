3bfc032566b59393ebf399838e6a53169f74bf70
heySanj <saprasad92@gmail.com>
2022-10-04 16:11:57 +1000

    Digital business card

--- exercises/name-card/server.py
+++ exercises/name-card/server.py
@@ -7,13 +7,8 @@ from markupsafe import escape # Escape input strings to prevent injection attack
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index.html')
 def home():
     return render_template('index.html')
-
-@app.route('/pages/<page_name>')
-def load_page(page_name):
-    return render_template("./pages/" + escape(page_name))
 
 if __name__ == '__main__':
     app.run(debug=True) # Run in debug mode to allow changes to be made without having to reload
