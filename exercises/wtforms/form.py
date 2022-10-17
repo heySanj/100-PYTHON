e12f6b3436e70e8ab701a4af1f4dd8ff215440e3
heySanj <saprasad92@gmail.com>
2022-10-17 15:32:28 +1000

    Bootstrap and WTForms within Flask

---
+++ exercises/wtforms/form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class MyForm(FlaskForm):
+    # Have a look at the basic fields of WTForms: https://wtforms.readthedocs.io/en/2.3.x/fields/#basic-fields
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)]) # When validation is not met, a list of errors can be generated and passed into HTML
+    submit = SubmitField(label='Log In')
---
+++ exercises/wtforms/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from form import MyForm
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = "secretkey"
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = MyForm()
+    # Validate the user input when they submit
+    if form.validate_on_submit():
+        if form.email.data == 'admin@email.com' and form.password.data == '12345678':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
